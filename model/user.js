15797fc7d8e8be341e3180626753eb996f3ed367
heicx <heicx_sudo@163.com>
2016-02-20 22:52:18 +0800

    v0.1.1

--- model/user.js
+++ model/user.js
@@ -1,4 +1,6 @@
 var when = require("when");
+var moment = require("moment");
+var utils = require("../helper/utils");
 
 module.exports = function(orm, db) {
 	var User = db.define("contract_user", {
@@ -7,26 +9,66 @@ module.exports = function(orm, db) {
 		user_password: String,
 		user_mobileNo: String,
 		user_type: Number,
-		user_status: Number,
+		user_status: {type: "integer", defaultValue: 1},
 		user_email: String,
-		create_time: Date,
-		update_time: Date
+		create_time: {type: "date", defaultValue: new Date()},
+		update_time: {type: "date", defaultValue: new Date()}
 	});
 
+
     /**
-     * 获取用户列表
+     * 用户查重
+     * @param params
+     */
+    User.findUserIsExist = function (params) {
+        var def = when.defer();
+
+        User.find({user_name: params.user_name}, function(err, user) {
+            if(!err)
+                if(user.length > 0) {
+                    def.reject("用户已存在哦");
+                }else {
+                    def.resolve(user);
+                }
+            else
+                def.reject("用户查重失败");
+        });
+
+        return def.promise;
+    }
+
+    /**
+     * 获取用户
      * @param params
      * @returns {promise}
      */
-    User.getAllUser = function(userType) {
+    User.getUser = function(params) {
         var def = when.defer();
+        var strCondition  = "", arrArgs = [];
+        var sql;
+        var arrOutput = {
+            user_type: {
+                keyword: "<>"
+            },
+            user_name: {
+                keyword: "like",
+                sign: ["%", "%"]
+            }
+        }
 
-        User.find({user_type: orm.ne(userType)}, function(err, userData) {
+        utils.ormFilter(params, arrOutput, function(str, arr) {
+            strCondition = str ? " where " + str : "";
+            arrArgs = arr;
+        });
+
+        sql = "select id, user_name, user_type, user_email, DATE_FORMAT(create_time, '%Y-%m-%d') as create_time from contract_user " + strCondition;
+
+        db.driver.execQuery(sql, arrArgs, function(err, users) {
             if(!err)
-                def.resolve(userData);
+                def.resolve(users);
             else
                 def.reject("获取用户列表失败");
-        })
+        });
 
         return def.promise;
     }
@@ -48,4 +90,24 @@ module.exports = function(orm, db) {
 
         return def.promise;
 	}
+
+    /**
+     * 添加用户
+     * @param params
+     * @returns {promise}
+     */
+    User.addUser = function(params) {
+        var def = when.defer();
+
+        User.create(params, function(err, user) {
+            if(user) {
+                user.create_time = moment(user.create_time).format("L");
+
+                def.resolve(user);
+            }else
+                def.reject("用户添加失败");
+        });
+
+        return def.promise;
+    }
 }
