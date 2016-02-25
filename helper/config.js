d1c14571cc99c3ab69c51322591cbe0c9c06023b
heicx <heicx_sudo@163.com>
2016-02-25 22:18:01 +0800

    v0.3.0

---
+++ helper/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "database": {
+        "protocol": "mysql",
+        "host": "127.0.0.1",
+        "user": "root",
+        "password": "root123",
+        "database": "contract_test"
+    }
+};
