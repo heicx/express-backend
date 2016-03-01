8f40fb13adad0c3fe8f9a1f74657ae44f7d722a6
heicx <heicx_sudo@163.com>
2016-03-01 15:53:35 +0800

    添加对Float类型的运算处理

--- helper/utils.js
+++ helper/utils.js
@@ -184,3 +184,85 @@ exports.paginationMath = function(pageNo, count) {
 
     return oPagination;
 }
+
+exports.Math = {
+    accDiv: function(arg1, arg2) {
+        var t1 = 0, t2 = 0, r1, r2, result;
+
+        try{
+            t1 = arg1.toString().split(".")[1].length;
+        }catch(e){}
+
+        try{
+            t2 = arg2.toString().split(".")[1].length;
+        }catch(e){}
+
+        with(Math){
+            var strSplit;
+
+            r1 = Number(arg1.toString().replace(".",""));
+            r2 = Number(arg2.toString().replace(".",""));
+
+            result = ((r1/r2) * pow(10, t2 - t1)) + "";
+            strSplit = result.split(".");
+
+            if(strSplit[1] && strSplit[1].length > 2)
+                return Number(strSplit[0] + "." + strSplit[1].substring(0, 2));
+            else
+                return Number(result);
+        }
+    },
+    accMul: function(arg1, arg2) {
+        var m = 0, s1 = arg1.toString(), s2 = arg2.toString();
+
+        try{
+            m += s1.split(".")[1].length;
+        }catch(e){}
+
+        try{
+            m+=s2.split(".")[1].length;
+        }catch(e){}
+
+        return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);
+    },
+    accAdd: function(arg1, arg2) {
+        var r1, r2, m;
+
+        try{
+            r1 = arg1.toString().split(".")[1].length;
+        }catch(e){
+            r1 = 0;
+        }
+
+        try{
+            r2 = arg2.toString().split(".")[1].length;
+        }catch(e){
+            r2 = 0;
+        }
+
+        m = Math.pow(10, Math.max(r1, r2));
+
+        return (arg1 * m + arg2 * m) / m;
+    },
+    accSub: function(arg1, arg2) {
+        var r1, r2, m, n;
+
+        try{
+            r1 = arg1.toString().split(".")[1].length;
+        }catch(e){
+            r1 = 0;
+        }
+
+        try{
+            r2 = arg2.toString().split(".")[1].length;
+        }catch(e){
+            r2 = 0;
+        }
+
+        m = Math.pow(10, Math.max(r1, r2));
+
+        n = (r1 >= r2) ? r1: r2;
+
+        return ((arg1 * m - arg2 * m) / m).toFixed(n);
+    }
+}
