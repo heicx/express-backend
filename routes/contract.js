9c7fb0aab1f4e6a57a8b58590531e43b6315659e
heicx <heicx_sudo@163.com>
2016-03-03 15:20:53 +0800

    修改保证金参数key名称

--- routes/contract.js
+++ routes/contract.js
@@ -100,7 +100,6 @@ var addContract = function(req, res) {
         end_time: req.body.endTime,
         create_time: new Date(),
         deposit: req.body.deposit,
-        deposit_remaining: req.body.deposit,
         contract_price: req.body.contractPrice,
         user_id: JSON.parse(req.session.user).id
     };
